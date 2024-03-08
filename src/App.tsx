ebdc5df47f46388e631e385e87e0c0dd2a5ae012
kevinka999 <kevinka999@gmail.com>
2024-03-08 07:34:10 -0300

    adding tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { DogSVG } from "./components";
-import "./global.css";
 
 export const App = () => {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Dog clicker</h1>
       <DogSVG />
     </div>
   );
